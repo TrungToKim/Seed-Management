0f45647f874438e5e9fa6ff9e33d3ad83abd58c5
TrungToKim <trungkimto@outlook.com>
2026-09-05 15:39:44 +0700

    sửa lại phần chat box ai hiện ở dưới đáy trang

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import Chatbox from "./Chatbox";
 
 export default function Layout() {
   const location = useLocation();
@@ -14,8 +13,6 @@ export default function Layout() {
         <Outlet />
       </main>
       {!isChatPage && <Footer />}
-      {/* Floating Chatbox Widget */}
-      {!isChatPage && <Chatbox />}
     </div>
   );
 }
