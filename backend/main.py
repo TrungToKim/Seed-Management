2e61e00a6886f1f85a2c7e6cc373d6e98a3f1dd6
TrungToKim <trungkimto@outlook.com>
2026-09-05 15:54:01 +0700

    sửa lại lỗi không tìm thấy port

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 # Entrypoint for FastAPI
+import os
 import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    # Render injection of $PORT environment variable
+    port = int(os.getenv("PORT", 8000))
+    is_dev = os.getenv("ENVIRONMENT", "development").lower() == "development"
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=is_dev)
